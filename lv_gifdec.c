a7ca9ad8a81f1fe61b66b98fbae89a8dbf390c3c
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-07-24 05:55:56 +0200

    clean up

--- lv_gifdec.c
+++ lv_gifdec.c
@@ -26,7 +26,6 @@ static void next_frame_task_cb(lv_task_t * t);
  *  STATIC VARIABLES
  **********************/
 static gd_GIF *gif;
-//static uint8_t buf[256*4 + 320 *320];
 static lv_obj_t * img;
 static lv_img_dsc_t imgdsc;
 
@@ -47,12 +46,6 @@ lv_obj_t * lv_gif_create(lv_obj_t * parent)
     imgdsc.header.cf = LV_IMG_CF_INDEXED_8BIT;
     imgdsc.header.h = gif->height;
     imgdsc.header.w = gif->width;
-
-//    uint32_t i;
-//    for(i = 0; i < 256; i++) {
-//        lv_color_t c = lv_color_make(gif->palette->colors[3 * i], gif->palette->colors[3 * i + 1], gif->palette->colors[3 * i + 2]);
-//        lv_img_buf_set_palette(&imgdsc, i, c);
-//    }
 
     img = lv_img_create(parent, NULL);
     lv_img_set_src(img, &imgdsc);
@@ -75,9 +68,7 @@ static void next_frame_task_cb(lv_task_t * t)
     }
 
     gd_get_frame(gif);
-//    gd_render_frame(gif, gif->canvas);
+    lv_obj_invalidate(img);
 
     counter = gif->gce.delay;
-
-    lv_obj_invalidate(img);
 }
