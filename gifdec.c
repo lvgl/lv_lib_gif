0dad02b4a9ba331ab3120adcf1aa1809de75f9b7
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-08-12 14:40:37 +0200

    halve memory usage and use lvgl's memory allocators

--- gifdec.c
+++ gifdec.c
@@ -107,7 +107,7 @@ static gd_GIF * gif_open(gd_GIF * gif_base)
     f_gif_read(gif_base, &aspect, 1);
     /* Create gd_GIF Structure. */
     gd_GIF *gif;
-    gif = calloc(1, sizeof(*gif) + 256 * 4 + 2 * width * height);
+    gif = lv_mem_alloc(sizeof(*gif) + 256 * 4 + width * height);
     if (!gif) {
         goto fail;
     } else {
@@ -241,7 +241,7 @@ new_table(int key_size)
 {
     int key;
     int init_bulk = MAX(1 << (key_size + 1), 0x100);
-    Table *table = malloc(sizeof(*table) + sizeof(Entry) * init_bulk);
+    Table *table = lv_mem_alloc(sizeof(*table) + sizeof(Entry) * init_bulk);
     if (table) {
         table->bulk = init_bulk;
         table->nentries = (1 << key_size) + 2;
@@ -262,7 +262,7 @@ add_entry(Table **tablep, uint16_t length, uint16_t prefix, uint8_t suffix)
     Table *table = *tablep;
     if (table->nentries == table->bulk) {
         table->bulk *= 2;
-        table = realloc(table, sizeof(*table) + sizeof(Entry) * table->bulk);
+        table = lv_mem_realloc(table, sizeof(*table) + sizeof(Entry) * table->bulk);
         if (!table) return -1;
         table->entries = (Entry *) &table[1];
         *tablep = table;
@@ -360,7 +360,7 @@ read_image_data(gd_GIF *gif, int interlace)
         } else if (!table_is_full) {
             ret = add_entry(&table, str_len + 1, key, entry.suffix);
             if (ret == -1) {
-                free(table);
+                lv_mem_free(table);
                 return -1;
             }
             if (table->nentries == 0x1000) {
@@ -392,7 +392,7 @@ read_image_data(gd_GIF *gif, int interlace)
         if (key < table->nentries - 1 && !table_is_full)
             table->entries[table->nentries - 1].suffix = entry.suffix;
     }
-    free(table);
+    lv_mem_free(table);
     f_gif_read(gif, &sub_len, 1); /* Must be zero! */
     f_gif_seek(gif, end, SEEK_SET);
     return 0;
@@ -488,7 +488,7 @@ void
 gd_close_gif(gd_GIF *gif)
 {
     f_gif_close(gif);
-    free(gif);
+    lv_mem_free(gif);
 }
 
 static bool f_gif_open(gd_GIF * gif, const void * path, bool is_file)
