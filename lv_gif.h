f532551d2f83daa5085666ade6f8c9c8083da48a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-09-14 10:49:48 +0200

    Update lv_gif.h

--- lv_gif.h
+++ lv_gif.h
@@ -31,8 +31,8 @@ extern "C" {
  * GLOBAL PROTOTYPES
  **********************/
 
-lv_obj_t * lv_gif_create_form_file(lv_obj_t * parent, const char * path);
-lv_obj_t * lv_gif_create_form_data(lv_obj_t * parent, const void * data);
+lv_obj_t * lv_gif_create_from_file(lv_obj_t * parent, const char * path);
+lv_obj_t * lv_gif_create_from_data(lv_obj_t * parent, const void * data);
 void lv_gif_restart(lv_obj_t * gif);
 
 /**********************
